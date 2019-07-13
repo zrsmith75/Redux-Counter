ea01e62d7f2caa5074ccaf92bdcb0e30ebc25310
Zac Smith <zrs3141592@gmail.com>
2019-07-13 11:30:02 -0600

    correct spelling

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -24,7 +24,7 @@ class Counter extends Component {
         <button
           onClick={() => {
             this.props.increment();
-            // Don't forget to invole () the function
+            // Don't forget to invoke () the function
           }}
         >
           +
